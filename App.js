a6a0fc46f26f21817a36c7b43a214f1cd2433bab
Guuh betete <gustavobetete@hotmail.com>
2023-02-08 20:39:36 -0300

    Textos generalizados

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import {
    Montserrat_400Regular,
     Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 import Cesta from './src/telas/Cesta';
+import mock from './src/mocks/cesta';
 
 export default function App() {
   const [fonteCarregada] = useFonts({
@@ -18,7 +19,7 @@ export default function App() {
   return (
     <SafeAreaView>
       <StatusBar />
-      <Cesta />
+      <Cesta {...mock} />
     </SafeAreaView>
   );
 }
